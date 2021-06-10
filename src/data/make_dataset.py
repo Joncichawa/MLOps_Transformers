8eb5c08b9f6a330ed0c0ff0b65a6d8c3200ed66c
Artur Przybysz <prz.artur@gmail.com>
2021-06-10 14:40:38 +0200

    Data loaders in progress

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,31 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from typing import Tuple
+
+from datasets import load_dataset
+from src.train_config import HyperParameters
+from torch.utils.data import DataLoader
+from transformers import DistilBertTokenizer  # TODO: Use before DataLoader!
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def prepare_loaders(_hyper_params: HyperParameters) -> Tuple[DataLoader, DataLoader]:
+    # See https://huggingface.co/docs/datasets/loading_datasets.html#cache-directory
+    # We don't need to save the dataset in data dir, as caching is handled by huggingface
+    dataset_raw = load_dataset("dbpedia_14")
+    dataset_raw = dataset_raw.remove_columns(column_names="title")
+
+    train_dataset = dataset_raw["train"]
+    test_dataset = dataset_raw["test"]
+
+    train_dataset.set_format(type='torch')
+    test_dataset.set_format(type='torch')
+
+    _train_loader = DataLoader(train_dataset, batch_size=_hyper_params.BATCH_SIZE)
+    _test_loader = DataLoader(test_dataset, batch_size=_hyper_params.BATCH_SIZE)
+
+    return _train_loader, _test_loader
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    hyper_params = HyperParameters(BATCH_SIZE=32)
+    train_loader, test_loader = prepare_loaders(hyper_params)
+    for x in test_loader:
+        print()
---
+++ src/train_config.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+
+@dataclass()
+class HyperParameters:
+    BATCH_SIZE: int
