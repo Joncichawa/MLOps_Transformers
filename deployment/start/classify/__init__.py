0f7f379299b7b7b797cc439dc7012e8925f56b6a
Jonatan Cichawa <joncichawa@protonmail.com>
2021-06-24 02:35:34 +0200

    finalized deployment code

--- deployment/start/classify/__init__.py
+++ deployment/start/classify/__init__.py
@@ -1,21 +1,23 @@
 import logging
-
 import azure.functions as func
 import json
-
+import ast
 # Import helper script
 from .predictonnx import predict_class_from_text
 
+headers = {
+              "Content-type": "application/json",
+              "Access-Control-Allow-Origin": "*"
+}
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
+       if req.params == {}: return func.HttpResponse(json.dumps({"3":"2"}, indent=4), headers = headers)
        input_text = req.params.get('text')
-       logging.info('Input text to classify received: ' + input_text)
 
+       logging.info(input_text)
+
+       input_text = ast.literal_eval(input_text)
        results = predict_class_from_text(input_text)
-
-       headers = {
-                     "Content-type": "application/json",
-                     "Access-Control-Allow-Origin": "*"
-       }
 
        return func.HttpResponse(json.dumps(results, indent=4), headers = headers)
 
--- deployment/start/classify/predictonnx.py
+++ deployment/start/classify/predictonnx.py
@@ -20,26 +20,17 @@ from torch.utils.data import DataLoader
 from transformers import DistilBertTokenizer
 import requests
 
-def load_labels(path):
-    with open(path) as f:
-        data = json.load(f)
-    return np.asarray(data)
-
 # Run the model on the backend
 d=os.path.dirname(os.path.abspath(__file__))
 modelfile=os.path.join(d , 'model.onnx')
 labelfile=os.path.join(d , 'labels.json')
 
 session = onnxruntime.InferenceSession(modelfile)
+np.set_printoptions(precision=None, suppress=True, formatter={'float_kind':'{:f}'.format})
 
-# get the name of the first input of the model
+# get the name of the inputs of the model
 input_name1 = session.get_inputs()[0].name
 input_name2 = session.get_inputs()[1].name  
-input_list = session.get_inputs() 
-output_list = session.get_outputs()
-output_name1 = session.get_outputs()[0].name
-
-labels = load_labels(labelfile)
 
 def predict_loader(texts: List[str], config: dict) -> List[Dict[str, Tensor]]:
     tokenizer = DistilBertTokenizer.from_pretrained(
@@ -77,50 +68,34 @@ def predict_class_from_text(input_text):
     config_path = "config.yaml"
     with open(config_path) as f:
         config = yaml.safe_load(f)
-    
-    logging.info(input_text)
-    # arr1 = bytes(input_text, 'utf-8')
-    # input_text = [" Gino Daniel Rea (born 18 September 1989 inTooting London) is an English motorcycle racer ofItalian descent.2010 was his first season in theSupersport World Championship riding for theIntermoto Czech Honda team. He won the 2009European Superstock 600 championship on a GeorgeWhite-backed Ten Kate Honda clinching the title bya single point in the final round at Portimão.Reawas a successful Motocross and Supermoto riderbefore switching to circuit racing in 2007."]
-    # arr3 = bytes(input_text, 'utf-8')
-    # logging.info(arr1)
-    # logging.info(arr3)
-    # logging.info(input_text)
 
-    pred_loader = predict_loader(input_text, config)
-    for x in pred_loader:
-        ids = x["input_ids"]
-        mask = x["attention_mask"]
-
-    logging.info("########## RAW RESULT ZEROOO ########")
-    raw_result = 0
-    logging.info(raw_result)
-    res = 0
-    logging.info("########## RES AFTER EXP ########3")
-    logging.info(res)
-
-    start = time.time()
-    raw_result = session.run(None, {input_name1: ids.numpy(), input_name2: mask.numpy()})
-    end = time.time()
-    np.set_printoptions(precision=None, suppress=True, formatter={'float_kind':'{:f}'.format})
-    logging.info("########## RAW RESULT ########3")
-    logging.info(raw_result)
-    res = torch.exp(torch.tensor(raw_result))
-    logging.info("########## RES AFTER EXP ########3")
-    logging.info(res.numpy())
-
-    inference_time = np.round((end - start) * 1000, 2)
-    idx = np.argmax(res.squeeze().numpy())
-    logging.info(res.squeeze().numpy())
-    logging.info(idx)
     labels = ['Company', 'EducationalInstitution', 'Artist', 'Athlete', 'OfficeHolder',
     'MeanOfTransportation', 'Building', 'NaturalPlace', 'Village', 'Animal',
     'Plant', 'Album', 'Film', 'WrittenWork']
 
+    pred_loader = predict_loader(input_text, config)
+    start = time.time()
+
+    results = []
+    for x in pred_loader:
+        ids = x["input_ids"]
+        mask = x["attention_mask"]
+
+        raw_result = session.run(None, {input_name1: ids.numpy(), input_name2: mask.numpy()})
+        res = torch.exp(torch.tensor(raw_result))
+        idx = np.argmax(res.squeeze().numpy()).astype(int)
+        results.append((str(idx), labels[idx]))
+
+    end = time.time()
+
+    inference_time = np.round((end - start) * 1000, 2)
+
     response = {
             'created': datetime.utcnow().isoformat(),
-            'prediction': labels[idx],
+            'predictions': results,
             'latency': inference_time,
     }
+    
     logging.info(f'returning {response}')
     return response
 
